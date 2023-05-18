dc2c77e95999f5e53db377643fbcac48c7e4d3f6
Polina Zakharyan <zakharyan.polina@gmail.com>
2023-05-18 21:38:07 +0300

    Fix compression for templog converter

--- task_08/convert_templog.py
+++ task_08/convert_templog.py
@@ -13,13 +13,13 @@ ARGPARSER.add_argument('--text', '-t', action='store_true',
     help='if option specified, conversion mode is text to binary, inverse otherwise')
 
 def to_text(input: bytearray) -> str:
-    arr = array('d')
+    arr = array('f')
     arr.frombytes(input)
     return ''.join(map(lambda x: str(round(x, 2)) + '\n', arr))
 
 def to_binary(input: str) -> bytearray:
     # compress data by converting the string number representation to true number
-    return bytearray(array('d', map(float, input.strip().split('\n'))))
+    return bytearray(array('f', map(float, input.strip().split('\n'))))
 
 
 if __name__ == "__main__":
