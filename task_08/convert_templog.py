c2b94714ecda9bb673a4c1ff776e7c5917bb9eeb
Polina Zakharyan <zakharyan.polina@gmail.com>
2023-05-18 19:52:37 +0300

    Add templog converter

---
+++ task_08/convert_templog.py
@@ -0,0 +1,36 @@
+import argparse
+from array import array
+
+ARGPARSER = argparse.ArgumentParser(
+    prog='weather templog converter',
+    description='convert weather temperature logs to/from binary format with data compression',
+)
+
+ARGPARSER.add_argument('input', nargs='?', type=str, help='input file')
+ARGPARSER.add_argument('output', nargs='?', type=str, help='output file', default='templog.o')
+
+ARGPARSER.add_argument('--text', '-t', action='store_true',
+    help='if option specified, conversion mode is text to binary, inverse otherwise')
+
+def to_text(input: bytearray) -> str:
+    arr = array('d')
+    arr.frombytes(input)
+    return ''.join(map(lambda x: str(round(x, 2)) + '\n', arr))
+
+def to_binary(input: str) -> bytearray:
+    # compress data by converting the string number representation to true number
+    return bytearray(array('d', map(float, input.strip().split('\n'))))
+
+
+if __name__ == "__main__":
+    from sys import argv
+    ns = ARGPARSER.parse_args(argv[1:])
+
+    if ns.text:
+        with open(ns.input, 'r') as infile:
+            with open(ns.output, 'wb') as outfile:
+                outfile.write(to_binary(infile.read()))
+    else:
+        with open(ns.input, 'rb') as infile:
+            with open(ns.output, 'w') as outfile:
+                outfile.write(to_text(infile.read()))
---
+++ task_08/test_converter.py
@@ -0,0 +1,13 @@
+from convert_templog import *
+
+def test_conversion():
+    TXT = '1.0\n1.1\n1.2\n1.3\n'
+    BIN = to_binary(TXT)
+
+    assert TXT == to_text(BIN)
+    assert BIN == to_binary(to_text(BIN))
+
+    print("test_conversion passed")
+
+if __name__ == '__main__':
+    test_conversion()
